2e5cbe2413f5b57714c540707285337c20fa458c
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-08-14 23:56:28 -0500

    add styles to OrientationLeftComponent

--- src/components/OrientationLeft/index.jsx
+++ src/components/OrientationLeft/index.jsx
@@ -5,21 +5,21 @@ import {SiFrontendmentor} from "react-icons/si"
 
 function OrientationLeft () {
     return (
-        <div className='orientation-left fixed left-4 bottom-0 z-10 text-light-slate '>
+        <div className='orientation-left fixed left-10 bottom-0 z-10 text-light-slate '>
             <ul className='social-style flex flex-col items-center after:content:" " after:bg-light-slate after:w-[1px] after:block after:h-[100px]'>
-                <li className=''>
-                    <a href="">
-                        <AiFillGithub className="h-7 w-7 mb-5"/>
+                <li className='pb-[17px]'>
+                    <a href="" className="pb-[0px]">
+                        <AiFillGithub className="h-7 w-7  inline-block transform transition-transform duration-200 ease-in hover:-translate-y-[6px] "/>
                     </a>
                 </li>
-                <li className=''>
-                    <a href="">
-                        <AiFillLinkedin className="h-7 w-7  mb-5"/>
+                <li className='pb-[17px]'>
+                    <a href="" className="">
+                        <AiFillLinkedin className="h-7 w-7    inline-block transform transition-transform duration-200 ease-in hover:-translate-y-[6px]"/>
                     </a>
                 </li>
-                <li className=' '>
-                    <a href="">
-                        <SiFrontendmentor className="h-7 w-7  mb-5"/>
+                <li className='pb-[30px]'>
+                    <a href="" >
+                        <SiFrontendmentor className="h-7 w-7    inline-block transform transition-transform duration-200 ease-in hover:-translate-y-[6px] "/>
                     </a>
                 </li>
               
