abd05a0bf3ce5cf06d5d51fbb5e21742f77f8c63
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-08-23 23:20:17 -0500

    add hover to imgs from projectCards

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -13,10 +13,18 @@ function ProjectCard  ({
 
   return (
     <div className="p-4 rounded-lg shadow-md bg-[#112240] w-[450px] ">
-        <figure>
-            <img className='h-[220px] w-[460px] object-cover rounded-md    ' src={imageSrc} alt={name} />
-       
+   
+   
+      <div className='relative cursor-pointer'> 
+      <figure>
+            <img className='h-[220px] w-[460px] object-cover rounded-md ' src={imageSrc} alt={name} />
+            <div className=" before:absolute before:bg-navy before:rounded-[5px]  before:inset-0 before:w-full before:h-full  before:opacity-[0.50] before:transition-opacity before:duration-300 before:hover:opacity-0">
+            <div className=" before:absolute before:bg-Cyan before:rounded-[5px]  before:inset-0 before:w-full before:h-full  before:opacity-[0.45] before:transition-opacity before:duration-300 before:hover:opacity-0"></div>
+
+            </div>
         </figure>
+          </div>
+       
     
       <h2 className='text-Cyan'>{name}</h2>
       <ul className={`contentClassName flex mb-[10px]`}> {/* Aplicar la clase de estilo de contenido específica */}
