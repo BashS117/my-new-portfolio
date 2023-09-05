96b6d84b5a9d3c875c6291a3efc0f5738617984c
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-09-04 21:59:06 -0500

    add links to OrientatiosRight/Left components

--- src/components/OrientationLeft/index.jsx
+++ src/components/OrientationLeft/index.jsx
@@ -8,17 +8,17 @@ function OrientationLeft () {
         <div className='orientation-left fixed left-10 bottom-0 z-10 text-light-slate '>
             <ul className='social-style flex flex-col items-center after:content:" " after:bg-light-slate after:w-[1px] after:block after:h-[100px]'>
                 <li className='pb-[17px]'>
-                    <a href="" className="pb-[0px]">
+                    <a href="https://github.com/BashS117" className="pb-[0px]">
                         <AiFillGithub className="h-7 w-7  inline-block transform transition-transform duration-200 ease-in hover:-translate-y-[6px] "/>
                     </a>
                 </li>
                 <li className='pb-[17px]'>
-                    <a href="" className="">
+                    <a href="https://www.linkedin.com/in/sebastian-rodriguez-mosquera-1549a4265" className="">
                         <AiFillLinkedin className="h-7 w-7    inline-block transform transition-transform duration-200 ease-in hover:-translate-y-[6px]"/>
                     </a>
                 </li>
                 <li className='pb-[30px]'>
-                    <a href="" >
+                    <a href="https://www.frontendmentor.io/profile/BashS117" >
                         <SiFrontendmentor className="h-7 w-7    inline-block transform transition-transform duration-200 ease-in hover:-translate-y-[6px] "/>
                     </a>
                 </li>
--- src/components/OrientationRight/index.jsx
+++ src/components/OrientationRight/index.jsx
@@ -3,7 +3,7 @@ function OrientationRight(){
     return(
         <div className='orientation-right flex flex-col items-center w-[40px] fixed left-[auto] right-4 top-0  text-light-slate before:content:"" before:block before:w-[1px] before:h-[130px] before:bg-light-slate'>
             <div className='email-style  relative cursor-pointer hover:text-Cyan'>
-                <a className="pt-[25px] pl-[7px] [writing-mode:vertical-lr]">sebasrm2000@gmail.com</a>
+                <a href="mailto:sebasrm2000@gmail.com?subject=Job%20Offer&body=Hello%20there%2C%20we%20contacted%20you%20from%20your%20website" className="pt-[25px] pl-[7px] [writing-mode:vertical-lr]">sebasrm2000@gmail.com</a>
             </div>
         </div>
     )
