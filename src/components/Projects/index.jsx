09f0ef94a1e45938519cf4ca6daeb1a02cd7d333
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-10-10 14:27:22 -0500

    feat 3 projects to projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -13,6 +13,26 @@ const Projects = () => {
           projectLink: "https://sol-y-luna-23.netlify.app",
           repositoryLink: "https://github.com/BashS117/solyluna"
         },
+        { 
+          type: "Personal Project", 
+          description:"A web application allows users to shorten valid URLs and manage a list of their shortened links. It supports easy link copying and provides error messages for empty input submissions. The interface is responsive, adapting to different screen sizes, and includes hover and focus states for interactive elements.", 
+          name: "Url-Shortening",
+          imageSrc: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b2aee1177918813.6525838f60a78.png",
+          features: ["React.js", "TailwindCSS", "API","Fetch"],
+          contentClassName: "text-lightest-slate", // Clase para estilos de contenido del proyecto 2
+          projectLink: "https://urlshorteninglinks.netlify.app/",
+          repositoryLink: "https://github.com/BashS117/url-shortening-landing-page"
+        },
+        { 
+          type: "Personal Project", 
+          description:"An anonymous question submission web app allows users send questions, questions persist on page reload, enabling viewing of other users. Each question has a dedicated route and a share button for shares on social media as a open-graph image.", 
+          name: "Anonimous Questions",
+          imageSrc: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b28e33177918813.65259eb0c34e3.png",
+          features: ["Next.js", "TailwindCSS", "Client/Server Component", "Supabase"],
+          contentClassName: "text-lightest-slate", // Clase para estilos de contenido del proyecto 2
+          projectLink: "https://anonimous-questions.vercel.app/",
+          repositoryLink: "https://github.com/BashS117/anonimous-questions"
+        },
         {
           type: "Personal Project",  
           name: "E-commerce product page",
@@ -33,6 +53,16 @@ const Projects = () => {
           contentClassName: "text-lightest-slate", // Clase para estilos de contenido del proyecto 2
           projectLink: "https://bashs117.github.io/todoapp/",
           repositoryLink: "https://github.com/BashS117/todoapp"
+        },
+        {
+          type: "Personal Project",  
+          name: "News Homepage",
+          description:"This was an excellent project to practice my CSS Grid skills. There were a couple of tricky decisions that I had to make.",
+          imageSrc: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/ff740d177918813.65256d2b00db7.png",
+          features: ["Next.js", "TailwindCSS", "Grid Areas" ],
+          contentClassName: "text-lightest-slate", // Clase para estilos de contenido del proyecto 2
+          projectLink: "https://news-homepage-by-bashcode.netlify.app/",
+          repositoryLink: "https://github.com/BashS117/news-homepage"
         },
     
         
